2b79e234f22b45a1b67ba7f44e555a0e41ba2ea7
Kares <CarlosEscribano113@gmail.com>
2025-04-22 18:57:00 +0200

    Diferenciación de cartas

--- Assets/Scripts/CardGameManager.cs
+++ Assets/Scripts/CardGameManager.cs
@@ -2,27 +2,34 @@ using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 using System.Linq;
+using System.Collections.Generic;
 
 public class CardGameManager : MonoBehaviour
 {
     [SerializeField] private CardInfo[] cardsInfo;
-    [SerializeField] private XRReferenceImageLibrary imagesLibrary;
-    [SerializeField] private ARTrackedImageManager trackedImageManager;
+    [SerializeField] private XRReferenceImageLibrary imageLibrary;
+    private ARTrackedImageManager trackedImageManager;
+    private List<XRReferenceImage> cardImagesList = new();
     private CardInfo[] randomizedInfo;
-    int index;
+    int index = 0;
 
     private void Awake()
     {
+        trackedImageManager = FindFirstObjectByType<ARTrackedImageManager>();
         randomizedInfo = cardsInfo.OrderBy(x => UnityEngine.Random.value).ToArray();
+        cardImagesList = imageLibrary.Where(img => img.name.ToLower().Contains("card")).ToList();
     }
 
     public void UpdateCardsInfo()
     {
-        foreach(var card in trackedImageManager.trackables)
+        foreach (var card in trackedImageManager.trackables)
         {
-            index = imagesLibrary.indexOf(card.referenceImage);
-            if(cardsInfo[index].sprite != null) card.GetComponent<Card>().SetSprite(randomizedInfo[index].sprite);
-            card.GetComponent<Card>().SetText(randomizedInfo[index].text);
+            if (cardImagesList.Contains(card.referenceImage))
+            {
+                index = cardImagesList.IndexOf(card.referenceImage);
+                if (cardsInfo[index].sprite != null) card.GetComponent<Card>().SetSprite(randomizedInfo[index].sprite);
+                card.GetComponent<Card>().SetText(randomizedInfo[index].text);
+            }
         }
     }
 }
