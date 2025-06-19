3edb75ab442d4a89815c7f4020da4ebed9a571ef
Kares <CarlosEscribano113@gmail.com>
2025-06-19 18:55:21 +0200

    Bugs visuales al rotar dispositivo solucionados

--- Assets/Scripts/GameUnits/PlayableUnit.cs
+++ Assets/Scripts/GameUnits/PlayableUnit.cs
@@ -26,7 +26,7 @@ public class PlayableUnit : MonoBehaviour
         if (trackable.referenceImage.name.ToLower().Contains("card")) gameUnit = card.GetComponent<Card>();
         else if (trackable.referenceImage.name.ToLower().Contains("piece")) gameUnit = piece.GetComponent<Piece>();
         else if (trackable.referenceImage.name.ToLower().Contains("board")) gameUnit = board.GetComponent<Board>();
-        //this.transform.localScale *= ScaleCameraFactor; //Se escala el objeto dependiendo de la calibración de la cámara del dispositivo
+        this.transform.localScale *= ScaleCameraFactor; //Se escala el objeto dependiendo de la calibración de la cámara del dispositivo
         initRotation = gameUnit.transform.localRotation; //Se obtiene la rotación inicial para hacer bien el detach (XT)
         StartCoroutine(EnableVisualizationWhenTracked()); //El objeto se verá una vez esté bien posicionado por el tracking
     }
--- Assets/Scripts/Settings&Configuration/CameraCalibrator.cs
+++ Assets/Scripts/Settings&Configuration/CameraCalibrator.cs
@@ -4,24 +4,46 @@ using UnityEngine.XR.ARFoundation;
 public class CameraCalibrator : MonoBehaviour
 {
     [SerializeField] private ARCameraManager arCameraManager; //Cámara asociada a la cámara del dispositivo
+    private Camera secondCam;
+    private ScreenOrientation prevOrientation;
 
     void Start()
     {
+        secondCam = GameObject.FindWithTag("SecondCam").GetComponent<Camera>();
         arCameraManager.frameReceived += OnCameraFrameReceived; //Cuando recibe un frame de la cámara del dispositivo esta se calibra
+        prevOrientation = Screen.orientation;
     }
 
-    void OnCameraFrameReceived(ARCameraFrameEventArgs args)
+    private void Update()
+    {
+        if(Screen.orientation != prevOrientation)
+        {
+            prevOrientation = Screen.orientation;
+            arCameraManager.frameReceived += UpdateSecondCam;
+        }
+    }
+
+    private void OnCameraFrameReceived(ARCameraFrameEventArgs args)
     {
         if (args.projectionMatrix.HasValue) 
         {
             Matrix4x4 projection = args.projectionMatrix.Value; //Accede a la matriz de proyección
             //Cambia el factor de escala de las PlayableUnits según la matriz de proyección
             //De esta forma los objetos se ven del mismo tamaño en distintos dispositivos
-            PlayableUnit.ScaleCameraFactor = 1/projection.m00; 
+            if (Screen.width <= Screen.height) //Depende de la orientación se toman valores distintos de la matriz para la escala de los objetos
+                PlayableUnit.ScaleCameraFactor = 1 / projection.m00; //Portrait coge la escala horizontal
+            else PlayableUnit.ScaleCameraFactor = 1 / projection.m11; //Landscape la vertical
             //Una vez se ha calculado ScaleCameraFactor la función se desuscribe del evento y el componente se desactiva
             //Para evitar consumo de recursos innecesarios
             arCameraManager.frameReceived -= OnCameraFrameReceived;
-            this.enabled = false;
+            secondCam.projectionMatrix = projection;
         }
+    }
+
+    private void UpdateSecondCam(ARCameraFrameEventArgs args)
+    {
+        Debug.Log("Second cam proj matrix updated");
+        secondCam.projectionMatrix = args.projectionMatrix.Value;
+        arCameraManager.frameReceived -= UpdateSecondCam;
     }
 }
