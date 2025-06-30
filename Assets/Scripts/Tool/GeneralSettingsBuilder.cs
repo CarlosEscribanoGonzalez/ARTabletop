58eabbc90039f16136f405f92202dfcc9f689744
Kares <CarlosEscribano113@gmail.com>
2025-07-01 00:18:43 +0200

    Tool manager medio bonito

--- Assets/Scripts/Tool/GeneralSettingsBuilder.cs
+++ Assets/Scripts/Tool/GeneralSettingsBuilder.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class GeneralSettingsBuilder : MonoBehaviour
 {
@@ -7,6 +8,12 @@ public class GeneralSettingsBuilder : MonoBehaviour
     [SerializeField] private Toggle diceToggle;
     [SerializeField] private Toggle wheelToggle;
     [SerializeField] private Toggle coinToggle;
+
+    [Header("Auto shuffle explanation: ")]
+    [SerializeField] private float explanationFadeSpeed = 1;
+    [SerializeField] private CanvasGroup explanationCanvasGroup;
+    [SerializeField] private Transform deployExplanationButton;
+    private bool shown = false;
 
     public bool AutoShuffle => autoShuffleToggle.isOn;
     public bool GameHasDice => diceToggle.isOn;
@@ -19,5 +26,33 @@ public class GeneralSettingsBuilder : MonoBehaviour
         diceToggle.isOn = gameInfo.gameHasDice;
         wheelToggle.isOn = gameInfo.gameHasWheel;
         coinToggle.isOn = gameInfo.gameHasCoins;
+    }
+
+    public void OnExplanationButtonClicked()
+    {
+        StopAllCoroutines();
+        deployExplanationButton.localScale = new Vector3(1, -deployExplanationButton.localScale.y, 1);
+        if (!shown) StartCoroutine(FadeInExplanation());
+        else StartCoroutine(FadeOutExplanation());
+    }
+
+    IEnumerator FadeInExplanation()
+    {
+        shown = true;
+        while(explanationCanvasGroup.alpha < 1)
+        {
+            explanationCanvasGroup.alpha += explanationFadeSpeed * Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    IEnumerator FadeOutExplanation()
+    {
+        shown = false;
+        while (explanationCanvasGroup.alpha > 0)
+        {
+            explanationCanvasGroup.alpha -= explanationFadeSpeed * Time.deltaTime;
+            yield return null;
+        }
     }
 }
